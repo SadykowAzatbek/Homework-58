616645187b4910433df567d728b1998c31d96772
SadykowAzatbek <sadykowazatbek@gmail.com>
2023-11-14 13:46:51 +0600

    Добавлена функция cancel, она закрывает Modal елемент со страницы

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import {useState} from 'react';
+import Modal from './components/Modal/Modal.tsx';
 import './App.css';
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
+
+  const cancel = () => {
+    setShowModal(false);
+  }
+
   return (
     <>
-
+      <Modal
+        show={showModal}
+        onClose={cancel}
+        title='Modal title'
+      >
+        <div
+          className='fs-5'
+          style={{position: 'absolute', top: '5px', right: '10px'}}
+          onClick={cancel}
+        >
+          X
+        </div>
+        <p>Modal content</p>
+      </Modal>
     </>
   );
 }
